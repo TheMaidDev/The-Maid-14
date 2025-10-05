eeca4255b814170a4f5d24ab45b54df0cad72da5
TotallyNotAHSKveez <110289449+HSKveez@users.noreply.github.com>
2025-10-05 20:12:28 +0600

    босс, я устал

--- Content.Client/PDA/PdaProgramItem.xaml.cs
+++ Content.Client/PDA/PdaProgramItem.xaml.cs
@@ -21,7 +21,7 @@ public sealed partial class PdaProgramItem : ContainerButton
 
     private readonly StyleBoxFlat _styleBox = new()
     {
-        BackgroundColor = Color.FromHex("#25252a"),
+        BackgroundColor = Color.FromHex("#2d100b"),
     };
 
     public Color BackgroundColor
