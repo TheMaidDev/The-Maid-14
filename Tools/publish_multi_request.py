fee600f6d978277b96e6955b7cbaa1cf694c66ba
TotallyNotAHSKveez <110289449+HSKveez@users.noreply.github.com>
2025-10-24 17:18:13 +0600

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -20,8 +20,8 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.goobstation.com/"
-FORK_ID = "GoobLRP"
+ROBUST_CDN_URL = "https://cdn.maidstation14.ru/"
+FORK_ID = "maid"
 
 def main():
     session = requests.Session()
