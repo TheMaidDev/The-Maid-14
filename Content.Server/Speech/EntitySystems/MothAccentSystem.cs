2122ca5c92e9db078bd93923d39a73cb93bcf269
TotallyNotAHSKveez <110289449+HSKveez@users.noreply.github.com>
2025-12-04 01:57:08 +0600

    speech ru locale

--- Content.Server/Speech/EntitySystems/MothAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/MothAccentSystem.cs
@@ -8,11 +8,20 @@
 using System.Text.RegularExpressions;
 using Content.Server.Speech.Components;
 using Content.Shared.Speech;
+using Robust.Shared.Random;
 
 namespace Content.Server.Speech.EntitySystems;
 
 public sealed class MothAccentSystem : EntitySystem
 {
+    //Maid edit start
+    [Dependency] private readonly IRobustRandom _random = default!;
+    private static readonly Regex RURegexLowerZH = new("ж+");
+    private static readonly Regex RURegexUpperZH = new("ж+");
+    private static readonly Regex RURegexLowerZ = new("ж+");
+    private static readonly Regex RURegexUpperZ = new("ж+");
+    //Maid edit end
+
     private static readonly Regex RegexLowerBuzz = new Regex("z{1,3}");
     private static readonly Regex RegexUpperBuzz = new Regex("Z{1,3}");
 
@@ -25,6 +34,13 @@ public sealed class MothAccentSystem : EntitySystem
     private void OnAccent(EntityUid uid, MothAccentComponent component, AccentGetEvent args)
     {
         var message = args.Message;
+
+        //Maid edit start
+        message = RURegexLowerZH.Replace(message, _random.Pick(new List<string>() { "жж", "жжж" }));
+        message = RURegexUpperZH.Replace(message, _random.Pick(new List<string>() { "ЖЖ", "ЖЖЖ" }));
+        message = RURegexLowerZ.Replace(message, _random.Pick(new List<string>() { "зз", "ззз" }));
+        message = RURegexUpperZ.Replace(message, _random.Pick(new List<string>() { "ЗЗ", "ЗЗЗ" }));
+        //Maid edit end
 
         // buzzz
         message = RegexLowerBuzz.Replace(message, "zzz");
